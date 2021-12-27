be7fed80820fdcd42b9c483a70542975f4bd2164
fladd <florian.krause@fladd.de>
2021-12-27 19:53:02 +0100

    Update zap.py

--- zap/zap.py
+++ zap/zap.py
@@ -126,6 +126,19 @@ class ResizingCanvas(tk.Canvas):
         self.bind("<Configure>", self.on_resize)
         self.height = self.winfo_reqheight()
         self.width = self.winfo_reqwidth()
+
+    def on_resize(self, event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width) / self.width
+        hscale = float(event.height) / self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all", 0, 0, wscale, hscale)
+
+
 class TrackTooltip:
     def __init__(self, treeview):
         self.treeview = treeview
